abf6757e613ce0011a6b3f402876bc53c128770b
Con-Shoc <cshocrylas@gmail.com>
2019-02-15 16:46:18 -0500

    Added Search Console and GA tracking

---
+++ config.rb
@@ -0,0 +1,26 @@
+# Compass CSS framework config file
+
+# require 'susy'
+# require 'modular-scale'
+# require 'sassy-buttons'
+# require 'breakpoint'
+# require 'compass-css-arrow'
+# require 'rgbapng'
+# require 'functions'
+# require 'mixins'
+# require 'vars'
+# require 'vendors'
+# # Require any additional compass plugins here.
+
+project_type = :stand_alone
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "/assets/sass"
+css_dir = "/assets/css"
+images_dir = "/assets/images"
+fonts_dir = "/assets/fonts"
+javascripts_dir = "/assets/js"
+line_comments = false
+preferred_syntax = :scss
+output_style = :expanded
+relative_assets = true
